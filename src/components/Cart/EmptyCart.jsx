7e52935160ad49eeb70ec16d442b3b2408ef3120
Dmytro <demetrgonchar@gmail.com>
2022-09-21 23:19:43 +0300

    Added empty cart page and condition for visible of this page

---
+++ src/components/Cart/EmptyCart.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import emptyCart from '../../assets/img/empty-cart.png'
+import { Link } from 'react-router-dom';
+
+const EmptyCart = () => {
+  return (
+    <div class="content">
+        <div class="container container--cart">
+          <div class="cart cart--empty">
+            <h2>Cart is empty <icon>😕</icon></h2>
+            <p>
+            You probably haven't ordered pizza yet.<br />
+               To order pizza, go to the main page.
+            </p>
+            <img src={emptyCart} alt="Empty cart" />
+            <Link to="/" class="button button--black">
+              <span>Back</span>
+            </Link>
+          </div>
+        </div>
+      </div>
+  )
+}
+
+export default EmptyCart
